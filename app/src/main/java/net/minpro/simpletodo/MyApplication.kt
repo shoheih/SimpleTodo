eb4670ef5c44b18bc9b3725ed27250b631fbbc28
林将平 <2329shohei@gmail.com>
2019-05-02 07:55:46 +0900

    prepare Realm

---
+++ app/src/main/java/net/minpro/simpletodo/MyApplication.kt
@@ -0,0 +1,12 @@
+package net.minpro.simpletodo
+
+import android.app.Application
+import io.realm.Realm
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
