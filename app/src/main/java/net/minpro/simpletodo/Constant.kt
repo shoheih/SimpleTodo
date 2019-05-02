4c8d3ce8f9308128458b73421ce6443f714b019e
林将平 <2329shohei@gmail.com>
2019-05-02 12:11:13 +0900

    opening edit fragment and transition edit activity from main activity

--- app/src/main/java/net/minpro/simpletodo/Constant.kt
+++ app/src/main/java/net/minpro/simpletodo/Constant.kt
@@ -1,2 +1,20 @@
 package net.minpro.simpletodo
 
+enum class IntentKey {
+    TITLE,
+    DEADLINE,
+    TASK_DETAIL,
+    IS_COMPLETED,
+    MODE_IN_EDIT
+}
+
+enum class ModeInEdit {
+    NEW_ENTRY,
+    EDIT
+}
+
+enum class FragmentTag {
+    MASTER,
+    DETAIL,
+    EDIT
+}
--- app/src/main/java/net/minpro/simpletodo/MainActivity.kt
+++ app/src/main/java/net/minpro/simpletodo/MainActivity.kt
@@ -1,29 +1,68 @@
 package net.minpro.simpletodo
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isTwoPane: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // SmartPhon or Tablet
+        if (R.id.container_detail != null) isTwoPane = true
+
+        // タスクの新規登録
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            goEditScreen("", "", "",false, ModeInEdit.NEW_ENTRY)
         }
+
+    }
+
+    private fun goEditScreen(
+        title: String,
+        deadline: String,
+        taskDetail: String,
+        isCompleted: Boolean,
+        mode: ModeInEdit) {
+
+        if (isTwoPane) {
+//            val fragmentManager = supportFragmentManager
+//            val fragmentTransaction = fragmentManager.beginTransaction()
+//            fragmentTransaction.add(R.id.container_detail, EditFragment.newInstance("1", "1"))
+//            fragmentTransaction.commit()
+
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container_detail, EditFragment.newInstance("1", "1"), FragmentTag.EDIT.toString()).commit()
+
+            return
+        }
+
+        val intent = Intent(this@MainActivity, EditActivity::class.java).apply {
+            putExtra(IntentKey.TITLE.name, title)
+            putExtra(IntentKey.DEADLINE.name, deadline)
+            putExtra(IntentKey.TASK_DETAIL.name, taskDetail)
+            putExtra(IntentKey.IS_COMPLETED.name, isCompleted)
+            putExtra(IntentKey.MODE_IN_EDIT.name, mode)
+        }
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        menu.apply {
+            findItem(R.id.menu_delete).isVisible = false
+            findItem(R.id.menu_edit).isVisible = false
+            findItem(R.id.menu_register).isVisible = false
+        }
         return true
     }
 
